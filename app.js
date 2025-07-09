9930af032fdf4771e3d48f27c211bf1386d757a9
OisifCode <clement.rollot71@gmail.com>
2025-07-09 18:11:58 +0200

    Some more troubleshooting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require ('express');
 const path = require('path');
 const app = express();
+const db = require('./models');
 
 app.set('view engine', 'ejs'); // Moteur de template EJS
 app.set('views', path.join(__dirname, 'views')); // Dossier contenant les vues
@@ -16,6 +17,19 @@ app.use('/', mainRoutes); // utilise le routeur principal
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Connecté sur le port ${port}.`)
-});
+const startServer = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connexion à la base OK');
+
+    await db.sequelize.sync({ alter: true });  // crée/maj tables sans perdre les données
+
+    app.listen(port, () => {
+      console.log(`Connecté sur le port ${port}.`);
+    });
+  } catch (error) {
+    console.error('Erreur lors de la connexion à la base:', error);
+  }
+};
+
+startServer();
