35b169822ea7b947a45b02842a35d2a6da66b541
OisifCode <clement.rollot71@gmail.com>
2025-07-09 17:42:54 +0200

    Production environment variables added

--- models/config.js
+++ models/config.js
@@ -7,7 +7,16 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT || 3000,
+    port: process.env.DB_PORT,
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
     logging: false
   }
 };
