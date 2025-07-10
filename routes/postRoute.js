7e50939281bd7e8a5735f21c6546f757a7ee5379
OisifCode <clement.rollot71@gmail.com>
2025-07-10 14:34:42 +0200

    Joi validation added for post submit

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const { Post } = require('../models');
+const { postSchema } = require('../validators/postValidator');
 
 // POST - Send a publication
 router.post('/submit', (req, res) => {
-    const { pseudo, histoire, allowVideo } = req.body;
-  
-    Post.create({
-      pseudo,
-      histoire,
-      allowVideo
+  console.log(req.body);
+
+  // Convertir la checkbox en booléen
+  const data = {
+    ...req.body,
+    allowVideo: req.body.allowVideo === 'on'
+  };
+
+  postSchema.validateAsync(data)
+    .then(validatedData => {
+      return Post.create(validatedData);
     })
-      .then(() => {
-        res.redirect('/');
-      })
-      .catch(err => {
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      if (err.isJoi) {
+        const errorMessages = err.details.map(detail => detail.message).join('<br>');
+        res.status(400).send(`Erreur de validation :<br>${errorMessages}`);
+      } else {
         console.error(err);
-        res.status(500).send('Erreur lors de la publication du récit.');
-      });
-  });
+        res.status(500).send("Erreur serveur lors de la publication.");
+      }
+    });
+});
 
 module.exports = router;
