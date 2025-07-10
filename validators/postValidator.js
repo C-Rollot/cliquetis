fcb49a39d3ef41f08261e7ec5d84ea371d564ac1
OisifCode <clement.rollot71@gmail.com>
2025-07-10 15:32:31 +0200

    CSS Modified, Win95 aesthetics added

--- validators/postValidator.js
+++ validators/postValidator.js
@@ -2,7 +2,10 @@ const Joi = require('joi');
 
 const postSchema = Joi.object({
   pseudo: Joi.string().max(50).allow('', null), // facultatif
-  histoire: Joi.string().min(10).required(),
+  histoire: Joi.string().min(10).required().messages({
+    'string.empty' : 'Le récit ne peut pas être vide.',
+    'string.min': 'Le récit doit contenir au moins 10 caractères.'
+  }),
   allowVideo: Joi.boolean().truthy('on').falsy('off').default(false)
 });
 
